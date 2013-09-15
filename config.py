119c8bb1ccd5cb322b196fa1fae97a693edf7a1c
lmas <lmasvensson@gmail.com>
2013-09-15 12:17:58 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+
+class CONFIG:
+    debug = True,
+    max_items = 50
+    output_dir = '.'
+    database_file = 'news.dat'
+    latest_file = 'index.html'
+    template = 'template.html'
+
+    feeds = {
+        'Google': (
+            'http://news.google.com/?output=rss',
+        ),
+        'Reddit': (
+            'http://www.reddit.com/.rss',
+        ),
+    }
